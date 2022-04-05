34426104891e33ef2ff01147d1d74c1217470833
micaela quijano <micaquijano04@gmail.com>
2022-04-04 22:37:36 -0300

    hook

--- src/componentes/ItemDetailContainer.js
+++ src/componentes/ItemDetailContainer.js
@@ -3,37 +3,31 @@ import { useParams } from "react-router-dom";
 import { useState } from "react";
 import { ItemDetail } from "./ItemDetail";
 import { productosIniciales } from "./ItemListContainer";
+import { toast } from "react-toastify";
+import { Spinner } from "react-bootstrap";
 
 export const ItemDetailContainer = () => {
   const [item, setItem] = useState();
   const { idProducto } = useParams();
-console.log(idProducto)
 
-
-useEffect(() => {
-    console.log("hi")
-   
-  });
-
-  /* useEffect(() => {
-      const pedido = new Promise((res) => {
-        setTimeout(() => {
-          res(productosIniciales);
-        }, 2000);
+  useEffect(() => {
+    const pedido = new Promise((res) => {
+      setTimeout(() => {
+        res(productosIniciales);
+      }, 2000);
+    });
+    pedido
+      .then((resultado) => {
+        const producto = resultado.find((p) => p.id === Number(idProducto));
+        if (producto !== undefined) setItem(producto);
+      })
+      .catch((error) => {
+        toast.error("error al cargar productos");
       });
-      console.log(pedido)
-      pedido
-        .then((resultado) => {
-          const producto = resultado.find(p=>p.id===idProducto)
-          console.log(11,producto)
-          if (producto!==undefined)
-          setItem(producto);
-        })
-        .catch((error) => {
-          toast.error("error al cargar productos");
-        })
-        
-    }
-  ); */
-  return <ItemDetail item={item} />;
+  }, [idProducto]);
+  return !!item ? (
+    <ItemDetail item={item} />
+  ) : (
+    <Spinner animation="border" variant="danger" />
+  );
 };
