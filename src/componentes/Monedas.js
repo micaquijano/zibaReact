9af77f6e49cefcc89fdeb63365335a047a1f7c5c
micaquijano <micaquijano04@gmail.com>
2022-04-10 19:21:06 -0300

    init with usd

--- src/componentes/Monedas.js
+++ src/componentes/Monedas.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import { Badge, Form, Image } from "react-bootstrap";
 import Select from "react-select";
-import { useMoney } from "../context/money";
+import { USD_SYMBOL, useMoney } from "../context/money";
 
 export const Monedas = () => {
-  const { symbols, setSymbol, dolarValue } = useMoney();
+  const { symbols, symbol, setSymbol, dolarValue } = useMoney();
 
   return (
     <>
@@ -15,6 +15,7 @@ export const Monedas = () => {
       <Select
         onChange={(m) => setSymbol(m.value)}
         options={symbols}
+        defaultValue={symbols[0]}
         formatOptionLabel={(symbol) => (
           <div className="country-option">
             <img width={30} height={25} src={symbol.image} />
