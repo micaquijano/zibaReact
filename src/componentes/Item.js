607dc3b9e307ddd61bec5c9df7955cfb0fab21ed
micaela quijano <micaquijano04@gmail.com>
2022-04-21 18:21:56 -0300

    card hover

--- src/componentes/Item.js
+++ src/componentes/Item.js
@@ -5,7 +5,7 @@ import { Precio } from "./Precio";
 export const Item = ({ producto }) => {
   return (
       <Link className="btn-card" to={`/producto/${producto.itemId}`}>
-        <Card className="card p-0 fixHeight">
+        <Card className="card p-0 fixHeight img-with-zoom">
           <Card.Img className="img p-1 fixHeight" variant="top" src={producto.img} />
           <Card.Body>
             <Card.Title>
